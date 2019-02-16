e969f4e02dec315f26b45fbc70f6a39b1bb65fee
Andy Dimnych <andydimnych@gmail.com>
2019-02-16 16:09:20 -0600

    Added history.sh file

---
+++ Files/History.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+##Andy Dimnych
+##ITMS 548
+##History placed in history.html file script
+
+cat /home/adimnych/.bash_history >> /home/adimnych/Files/History.html
+## copy the history from bash into my history.html file
+## since this file is -R--R--R--, I've included the same command inside the bash_rc.file for it to be automatically updated everytime you write different commands in a new terminal, and then open a new terminal, the history gets updated. 
